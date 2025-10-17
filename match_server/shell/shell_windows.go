4d138debe3c53d5a1c84968373c4bd884793766f
724789975 <724789975@qq.com>
2025-10-17 17:51:09 +0800

    整体加入匹配功能

---
+++ match_server/shell/shell_windows.go
@@ -0,0 +1,21 @@
+package shell
+
+import (
+	common_config "match_server/config"
+	"os"
+	"syscall"
+
+	"github.com/cloudwego/kitex/pkg/klog"
+)
+
+func StartCmd(params string) {
+	cmdPath := "C:\\Windows\\System32\\cmd.exe"
+	if _, err := os.StartProcess(cmdPath, []string{cmdPath, "/k", common_config.Get("exe_path.windows").(string), params}, &os.ProcAttr{
+		Sys:   &syscall.SysProcAttr{HideWindow: false},
+		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+	}); err != nil {
+		klog.Errorf("start cmd %s failed, err: %v", common_config.Get("exe_path.windows").(string), err)
+	}
+
+	klog.Infof("start cmd %s success", common_config.Get("exe_path.windows").(string))
+}

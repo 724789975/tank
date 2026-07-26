6383b6342a40064585ebeaf942c0de45cf16a92c
724789975 <724789975@qq.com>
2026-07-26 19:46:31 +0800

    chore(build): 优化Docker镜像构建脚本
    - 删除了旧的打包文件item-manager.tar.gz
    - 移除旧版本的Docker镜像item-manager:v1.0.0
    - 直接使用默认Dockerfile构建item-manager:v1.0.0镜像
    - 保存构建好的镜像为tar.gz文件供后续使用
    - 简化批处理脚本结构，去除多余变量和错误处理逻辑

--- item_manager/build-image.bat
+++ item_manager/build-image.bat
@@ -1,17 +1,5 @@
-@echo off
-setlocal
-
-set IMAGE_NAME=item_manager:latest
-set DOCKERFILE_PATH=./Dockerfile
-
-echo Building Docker image: %IMAGE_NAME%
-docker build -t %IMAGE_NAME% -f %DOCKERFILE_PATH% .
-
-if %ERRORLEVEL% equ 0 (
-    echo Docker image built successfully: %IMAGE_NAME%
-) else (
-    echo Failed to build Docker image
-    exit /b 1
-)
-
-endlocal
+REM 删除打包文件
+del /F /Q item-manager.tar.gz
+docker rmi item-manager:v1.0.0
+docker build -t item-manager:v1.0.0 .
+docker save -o item-manager.tar.gz item-manager:v1.0.0
